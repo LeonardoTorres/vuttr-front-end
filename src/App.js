fa1f3e3436443f256fe5acc0d05b15e527096df5
Leonardo Torres <leonardotorres9@gmail.com>
2019-03-27 19:47:09 -0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Vuttr App</h3> 
+      </div>
+    );
+  }
+}
+
+export default App;
